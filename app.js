6984a08ca2a8acbf2dcdefa53f22822945db4401
Jason Schmitt <schmitty890@gmail.com>
2019-11-06 19:25:03 -0500

    uncommenting passport routes to create update and delete users

--- app.js
+++ app.js
@@ -136,13 +136,13 @@ app.get('/', homeController.index);
 // app.post('/signup', userController.postSignup);
 // app.get('/contact', contactController.getContact);
 // app.post('/contact', contactController.postContact);
-// app.get('/account/verify', passportConfig.isAuthenticated, userController.getVerifyEmail);
-// app.get('/account/verify/:token', passportConfig.isAuthenticated, userController.getVerifyEmailToken);
-// app.get('/account', passportConfig.isAuthenticated, userController.getAccount);
-// app.post('/account/profile', passportConfig.isAuthenticated, userController.postUpdateProfile);
-// app.post('/account/password', passportConfig.isAuthenticated, userController.postUpdatePassword);
-// app.post('/account/delete', passportConfig.isAuthenticated, userController.postDeleteAccount);
-// app.get('/account/unlink/:provider', passportConfig.isAuthenticated, userController.getOauthUnlink);
+app.get('/account/verify', passportConfig.isAuthenticated, userController.getVerifyEmail);
+app.get('/account/verify/:token', passportConfig.isAuthenticated, userController.getVerifyEmailToken);
+app.get('/account', passportConfig.isAuthenticated, userController.getAccount);
+app.post('/account/profile', passportConfig.isAuthenticated, userController.postUpdateProfile);
+app.post('/account/password', passportConfig.isAuthenticated, userController.postUpdatePassword);
+app.post('/account/delete', passportConfig.isAuthenticated, userController.postDeleteAccount);
+app.get('/account/unlink/:provider', passportConfig.isAuthenticated, userController.getOauthUnlink);
 
 /**
  * API examples routes.
