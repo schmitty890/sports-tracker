cd3474de6ef59ec606e2888f5ce01f64b655af0b
jschmitt <schmitty890@gmail.com>
2019-11-09 20:29:49 -0500

    enabling logout functionality from passport

--- app.js
+++ app.js
@@ -127,7 +127,7 @@ app.use('/', express.static(path.join(__dirname, 'public'), { maxAge: 3155760000
 app.get('/', homeController.index);
 // app.get('/login', userController.getLogin);
 // app.post('/login', userController.postLogin);
-// app.get('/logout', userController.logout);
+app.get('/logout', userController.logout);
 // app.get('/forgot', userController.getForgot);
 // app.post('/forgot', userController.postForgot);
 // app.get('/reset/:token', userController.getReset);
