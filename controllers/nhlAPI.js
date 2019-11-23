4d82b8decc6e58e9c7e19cab16cab89b8791ab60
jschmitt <schmitty890@gmail.com>
2019-11-22 22:09:16 -0500

    nhl api to determine if team is playing a game today or not

---
+++ controllers/nhlAPI.js
@@ -0,0 +1,23 @@
+const axios = require('axios');
+
+/**
+ * GET /gameToday
+ * Determine if a team is playing today
+ * returns true or false
+ */
+exports.gameToday = async (teamID) => {
+  const url = `https://statsapi.web.nhl.com/api/v1/schedule?teamId=${teamID}`;
+  return axios.get(url)
+    .then(({ data }) => {
+      let gameToday = data.totalGames;
+
+      if (gameToday === 1) {
+        gameToday = true;
+      } else {
+        gameToday = false;
+      }
+
+      return gameToday;
+    })
+    .catch(() => Promise.reject(new Error('There was an error while getting player summary')));
+};
--- controllers/subscribe.js
+++ controllers/subscribe.js
@@ -1,11 +1,13 @@
 const User = require('../models/User');
 const Subscribe = require('../models/Subscribe');
+const nhlAPI = require('./nhlAPI');
 
 /**
  * POST /subscribeToTeam
  * Subscribe to a sports team
  */
 exports.postSubscribeToTeam = (req, res, next) => {
+  // console.log(req.body);
   Subscribe.create(req.body)
     .then((dbSubscribe) => User.findOneAndUpdate({ _id: req.params.id }, { $push: { subscribed: dbSubscribe._id } }, { new: true }))
     .then((dbUser) => {
@@ -21,8 +23,8 @@ exports.postSubscribeToTeam = (req, res, next) => {
  * Subscribe to a sports team
  */
 exports.getSubscribedTeams = (req, res, next) => {
-  console.log('getSubscribedTeams');
-  console.log(req.body);
+  // console.log('getSubscribedTeams');
+  // console.log(req.body);
   User.findOne({ _id: req.params.id })
     .populate('subscribed')
     .then((dbUser) => {
@@ -37,10 +39,19 @@ exports.getSubscribedTeams = (req, res, next) => {
  * GET /
  * Dashboard page.
  */
-exports.index = async (req, res) => {
+exports.index = (req, res) => {
   if (!req.user) {
     return res.redirect('/login');
   }
+
+  if (req.user.subscribed.length === 0) {
+    return res.redirect('/account');
+  }
+
+  let hbsObject = {
+    user: req.user
+  };
+
   User
     .findOne({ _id: req.user._id })
     .populate({
@@ -51,14 +62,38 @@ exports.index = async (req, res) => {
       if (err) {
         console.log(err);
       } else {
-        const hbsObject = {
+        hbsObject = {
           user: subscriptions
         };
-        console.log(hbsObject);
-        res.render('index', {
-          title: 'Home',
-          hbsObject
-        });
+
+        /**
+         * render the dashboard page
+         */
+        const renderDashboard = () => {
+          res.render('index', {
+            hbsObject
+          });
+        };
+
+        /**
+         * doTeamsHaveGamesToday takes users subscribed nhl teams and fetches if they have a game today or not
+         *
+         */
+        const doTeamsHaveGamesToday = async () => {
+          const subscribers = hbsObject.user.subscribed;
+
+          // TODO: we can remove this await outside of the for loop but it works for the purpose of updating the ture/false if a team is playing a game, eslint is throwing error and we should fix LINK: https://eslint.org/docs/rules/no-await-in-loop
+          /* eslint-disable */
+          for (let i = 0; i < subscribers.length; i++) {
+            const gameToday = await nhlAPI.gameToday(subscribers[i].body.teamID);
+            subscribers[i].body.gameToday = gameToday;
+          }
+          /* eslint-enable */
+
+          renderDashboard();
+        };
+
+        doTeamsHaveGamesToday();
       }
     });
 };
